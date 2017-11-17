0469c8e9233e59fc5a2ea374033c73eebbab0cf3
tiddnet <todd.c.campbell@gmail.com>
2017-11-17 02:51:09 -0500

    fixing example

--- search/main.tf
+++ search/main.tf
@@ -1,3 +1,19 @@
-output "my-source-ami" {
-  value = "${data.aws_ami.deploy.id}"
+data "aws_ami" "deploy" {
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+
+  filter {
+    name   = "tag:Product"
+    values = ["MyProductName"]
+  }
+
+  filter {
+    name   = "tag:Deploy"
+    values = ["ALL"]
+  }
+
+  most_recent = true
+  owners      = ["self"]
 }
